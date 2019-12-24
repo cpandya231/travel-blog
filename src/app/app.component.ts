5a5f9bbc5c9e6e72435905ea3a4f19c16eafa83b
u8zfvf <chintan.pandya@siemens.com>
2019-12-24 14:26:22 +0530

    Add navbar transition on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  
+  
   title = 'css-bootstrap';
+  ngAfterViewInit() {
+    $(document).ready(function() {
+      
+        $(window).scroll(function() {
+           if($(this).scrollTop() > 500) { 
+             
+               $('.navbar').addClass('scrolled');
+           } else {
+               $('.navbar').removeClass('scrolled');
+           }
+        });
+    });
+}
+  
 }
